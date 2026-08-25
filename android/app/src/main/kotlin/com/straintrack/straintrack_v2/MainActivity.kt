af495bfa2716abff3db3fe6f5250fb9835079d0c
phrastz <phrastz@gmail.com>
2026-08-25 09:52:25 +0700

    fix: add gitignore, remove flutter files from tracking

--- android/app/src/main/kotlin/com/straintrack/straintrack_v2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.straintrack.straintrack_v2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/models.dart
+++
@@ -1,4 +0,0 @@
-export 'user.dart';
-export 'organization.dart';
-export 'farm.dart';
-export 'disease_report.dart';
